594e6b7a221a7834147257e05704aaecef5dd73a
qianfeng <apple@aofel.local>
2014-04-14 14:21:50 +0800

    Initial Commit

---
+++ Custom_OperationTests/Custom_OperationTests.m
@@ -0,0 +1,34 @@
+//
+//  Custom_OperationTests.m
+//  Custom_OperationTests
+//
+//  Created by qianfeng on 14-4-14.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Custom_OperationTests : XCTestCase
+
+@end
+
+@implementation Custom_OperationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
